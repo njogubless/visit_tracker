58215e274ff3d95a9e8264a304366074af16047d
njogubless <pnjogubless@gmail.com>
2025-05-31 21:32:29 +0300

    adding the font

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Roboto',
       ),
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
